5e6cc4e3ef00e4bcdb23d91facdb5dbf145e5266
Your Name <you@example.com>
2019-01-27 22:25:41 +0100

    add SkillActivity

---
+++ app/src/main/java/e/brucelee/chapter5/SkillActivity.kt
@@ -0,0 +1,12 @@
+package e.brucelee.chapter5
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SkillActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_skill)
+    }
+}
