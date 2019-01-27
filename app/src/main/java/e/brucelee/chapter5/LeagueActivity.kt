636ee771f457d2457a125a863097d0471a595203
Your Name <you@example.com>
2019-01-27 18:17:38 +0100

    add BaseActivity

--- app/src/main/java/e/brucelee/chapter5/LeagueActivity.kt
+++ app/src/main/java/e/brucelee/chapter5/LeagueActivity.kt
@@ -3,7 +3,7 @@ package e.brucelee.chapter5
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
