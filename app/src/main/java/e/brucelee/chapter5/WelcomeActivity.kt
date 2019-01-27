ba440ef55985a0c49494fe89e6ec8b3fdb9f8580
Your Name <you@example.com>
2019-01-27 14:33:51 +0100

    add Welcomescreen

--- app/src/main/java/e/brucelee/chapter5/WelcomeActivity.kt
+++ app/src/main/java/e/brucelee/chapter5/WelcomeActivity.kt
@@ -3,10 +3,10 @@ package e.brucelee.chapter5
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_welcome)
     }
 }
